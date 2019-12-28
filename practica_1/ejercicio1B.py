0b47af6ee5f40f89c84f5521e20ca111027f90bf
mauro40ar <mauro39ar@gmail.com>
2019-12-28 15:11:20 -0300

    "ejercicios "

---
+++ practica_1/ejercicio1B.py
@@ -0,0 +1,17 @@
+#I
+
+summers=input("ingrese una oracion: ")
+summer=summers.upper()
+print(summer)
+
+#II
+
+sky="HOLA"
+skys=sky.lower()
+print (skys)
+
+#III
+
+grey=" esto es un ejercicio"
+greys=grey.replace("ejercicio","prueba")
+print (greys)
---
+++ practica_1/ejercicio1a.py
@@ -0,0 +1,3 @@
+port=22
+banner="SSH port"
+print("{+] checking for %s on port %s"%(banner,port))
